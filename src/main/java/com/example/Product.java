c2f635f0aff0b2d72fd5730b927d7409476932cb
jorge <jorgebotero190@gmail.com>
2026-02-18 22:02:11 -0500

    Crea Contructor por defecto

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -7,11 +7,29 @@ public class Product {
     private String Descripcion;
     private double Precio;
 
+    public Product() {
+    this.id = "";
+    this.Nombre = "";
+    this.Descripcion = "";
+    this.Precio = 0.0;
+    }
+
    public Product(String id, String Nombre, String Descripcion, double Precio) {
     this.id = id;
     this.Nombre = Nombre;
     this.Descripcion = Descripcion;
     this.Precio = Precio;
     }
-
+    public String getId() {
+        return id;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+    public String getDescripcion() {
+        return Descripcion;
+    }
+    public double getPrecio() {
+        return Precio;
+    }
 }
