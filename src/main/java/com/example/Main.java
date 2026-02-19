2ec4163a3210abb91575a221eff0eda464b1e9bc
jorge <jorgebotero190@gmail.com>
2026-02-18 22:37:14 -0500

    Crea Objeto con constructor por defecto

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,13 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+
+              Product product1 = new Product();
+              System.out.println("Producto 1:");
+              
+
+
+
+
+    }}
